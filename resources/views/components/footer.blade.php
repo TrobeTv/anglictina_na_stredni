e63d63837d6b4720aaa053edc15a99a8970feac8
Ondřej Srostlík <ondra.srostlik@gmail.com>
2025-08-04 01:41:44 +0200

    FIX - footer - background svg image

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,10 +1,13 @@
-<footer class="bg-[#969696] text-white pt-12">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+<footer class="relative bg-[#767676] text-white pt-12 overflow-hidden">
+    <div class="absolute inset-0 z-0">
+        <svg id="visual" class="h-full w-full object-cover" viewBox="0 0 1920 540" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"><rect x="0" y="0" width="1920" height="540" fill="#767676"></rect><defs><linearGradient id="grad1_0" x1="71.9%" y1="0%" x2="100%" y2="100%"><stop offset="-21.111111111111104%" stop-color="#767676" stop-opacity="1"></stop><stop offset="121.1111111111111%" stop-color="#767676" stop-opacity="1"></stop></linearGradient></defs><defs><linearGradient id="grad2_0" x1="0%" y1="0%" x2="28.1%" y2="100%"><stop offset="-21.111111111111104%" stop-color="#767676" stop-opacity="1"></stop><stop offset="121.1111111111111%" stop-color="#767676" stop-opacity="1"></stop></linearGradient></defs><g transform="translate(1920, 0)"><path d="M0 515C-92.7 507.4 -185.4 499.7 -265 459C-344.6 418.3 -411 344.5 -454.7 262.5C-498.3 180.5 -519.2 90.2 -540 0L0 0Z" fill="#696969"></path></g><g transform="translate(0, 540)"><path d="M0 -540C93.7 -524 187.4 -508.1 267.5 -463.3C347.6 -418.6 414.2 -345 455.5 -263C496.9 -181 512.9 -90.5 529 0L0 0Z" fill="#696969"></path></g></svg>
+    </div>
+
+    <div class="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
 
             <div class="mb-8 md:mb-0">
-                <div class="w-[153px] h-[153px] bg-[#D9D9D9] rounded-full mx-auto md:mx-0">
-                    </div>
+                <div class="w-[153px] h-[153px] bg-[#D9D9D9] rounded-full mx-auto md:mx-0"></div>
                 <p class="mt-6 text-xl font-bold text-center md:text-left">
                     ANGLIČTINA NA STŘEDNÍ
                 </p>
@@ -27,7 +30,7 @@
                             <li class="mb-2"><a href="#" class="hover:underline">O nás</a></li>
                             <li class="mb-2"><a href="#" class="hover:underline">O projektu</a></li>
                             <li class="mb-2"><a href="#" class="hover:underline">Administrační prostředí</a></li>
-                        </ul>
+                            </ul>
                     </div>
                     <div>
                         <h3 class="text-2xl font-bold text-[#f8b900] mb-4">Podmínky</h3>
@@ -38,7 +41,7 @@
                             <li class="mb-2"><a href="#" class="hover:underline">Cookies</a></li>
                             <li class="mb-2"><a href="#" class="hover:underline">Obchodní podmínky</a></li>
                             <li class="mb-2"><a href="#" class="hover:underline">Zpracování osobních údajů</a></li>
-                        </ul>
+                            </ul>
                     </div>
                 </div>
             </div>
@@ -46,7 +49,7 @@
             <div>
                 <h3 class="text-2xl font-bold text-white mb-4">Sledujte nás</h3>
                 <ul>
-                    <li class="flex items-center mb-3">
+                     <li class="flex items-center mb-3">
                         <svg class="w-6 h-6 mr-3 fill-[#f8b900]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 37 37"><path d="M18.5041 9.01367C13.2544 9.01367 9.01991 13.2491 9.01991 18.5C9.01991 23.7509 13.2544 27.9863 18.5041 27.9863C23.7539 27.9863 27.9883 23.7509 27.9883 18.5C27.9883 13.2491 23.7539 9.01367 18.5041 9.01367ZM18.5041 24.6674C15.1116 24.6674 12.3381 21.9015 12.3381 18.5C12.3381 15.0985 15.1033 12.3326 18.5041 12.3326C21.9049 12.3326 24.6701 15.0985 24.6701 18.5C24.6701 21.9015 21.8967 24.6674 18.5041 24.6674ZM30.5885 8.62563C30.5885 9.8558 29.5979 10.8383 28.3763 10.8383C27.1464 10.8383 26.1641 9.84754 26.1641 8.62563C26.1641 7.40372 27.1547 6.41298 28.3763 6.41298C29.5979 6.41298 30.5885 7.40372 30.5885 8.62563ZM36.87 10.8713C36.7297 7.90734 36.0528 5.28188 33.8819 3.11877C31.7193 0.955651 29.0944 0.278646 26.1311 0.130035C23.077 -0.0433449 13.923 -0.0433449 10.8689 0.130035C7.91383 0.270389 5.28896 0.947395 3.11807 3.11051C0.947184 5.27362 0.278583 7.89908 0.130006 10.863C-0.0433352 13.9178 -0.0433352 23.0739 0.130006 26.1287C0.270329 29.0927 0.947184 31.7181 3.11807 33.8812C5.28896 36.0443 7.90558 36.7214 10.8689 36.87C13.923 37.0433 23.077 37.0433 26.1311 36.87C29.0944 36.7296 31.7193 36.0526 33.8819 33.8812C36.0446 31.7181 36.7214 29.0927 36.87 26.1287C37.0433 23.0739 37.0433 13.9261 36.87 10.8713ZM32.9244 29.4064C32.2806 31.0246 31.0342 32.2713 29.4081 32.9235C26.9731 33.8895 21.195 33.6666 18.5041 33.6666C15.8132 33.6666 10.0269 33.8812 7.60017 32.9235C5.98232 32.2795 4.73592 31.0329 4.08383 29.4064C3.11807 26.9708 3.34094 21.1915 3.34094 18.5C3.34094 15.8085 3.12632 10.0209 4.08383 7.59361C4.72766 5.9754 5.97407 4.72872 7.60017 4.07648C10.0352 3.11051 15.8132 3.33343 18.5041 3.33343C21.195 3.33343 26.9813 3.11877 29.4081 4.07648C31.0259 4.72046 32.2723 5.96714 32.9244 7.59361C33.8902 10.0292 33.6673 15.8085 33.6673 18.5C33.6673 21.1915 33.8902 26.9791 32.9244 29.4064Z"/></svg>
                         <a href="#" class="hover:underline">@anglictinanastredni</a>
                     </li>
@@ -58,12 +61,13 @@
                         <svg class="w-6 h-6 mr-3 fill-[#f8b900]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 37 37"><path d="M34.3571 0H2.6346C1.18103 0 0 1.19754 0 2.66763V34.3324C0 35.8025 1.18103 37 2.6346 37H34.3571C35.8107 37 37 35.8025 37 34.3324V2.66763C37 1.19754 35.8107 0 34.3571 0ZM11.1826 31.7143H5.69866V14.0567H11.1908V31.7143H11.1826ZM8.44062 11.6451C6.68147 11.6451 5.26094 10.2163 5.26094 8.4654C5.26094 6.71451 6.68147 5.28571 8.44062 5.28571C10.1915 5.28571 11.6203 6.71451 11.6203 8.4654C11.6203 10.2246 10.1998 11.6451 8.44062 11.6451ZM31.7391 31.7143H26.2551V23.125C26.2551 21.0768 26.2138 18.4422 23.4058 18.4422C20.5482 18.4422 20.1105 20.6721 20.1105 22.9763V31.7143H14.6266V14.0567H19.8875V16.4683H19.9618C20.6969 15.0808 22.4891 13.619 25.1567 13.619C30.7067 13.619 31.7391 17.2777 31.7391 22.0348V31.7143Z"/></svg>
                         <a href="#" class="hover:underline">@anglictinanastredni</a>
                     </li>
-                </ul>
+                    </ul>
             </div>
+
         </div>
     </div>
 
-    <div class="mt-8 bg-[#f8b900] py-4">
+    <div class="relative z-10 mt-8 bg-[#f8b900] py-4">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <p class="text-center text-sm font-extralight text-black">
                 © {{ date('Y') }} Angličtina na střední. Všechna práva vyhrazena.
